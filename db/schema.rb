225c392a8403c5f44a303347f10edfa12cc3836f
NeedsAUsername <kevinchi118@gmail.com>
2018-09-07 15:41:13 -0400

    writes and passes basic tests for user model

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,9 @@
 ActiveRecord::Schema.define(version: 2018_09_07_171948) do
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,26 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
   let(:user) {
     User.create(
-      name: "TestingName",
+      name: "ExampleUser",
       email: "Example@example.com",
       password: "pass"
     )
   }
+
+  it "is valid with a name, email, password" do
+    expect(user).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    expect(User.new(email: "email", password: "pass")).not_to be_valid
+  end
+
+  it 'is not valid without an email' do
+    expect(User.new(name: "name", password: "pass")).not_to be_valid
+  end
+
+  it 'is not valid without a password' do
+    expect(User.new(name: "name", email: "email")).not_to be_valid
+  end
 
 end
