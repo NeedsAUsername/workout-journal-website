abc03928307d6c06e8964d034351ab69a1eb5318
NeedsAUsername <kevinchi118@gmail.com>
2018-09-15 23:03:23 -0400

    adds CA certs

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-end 
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], {
+    client_options: { ssl: {
+        ca_file: '/usr/lib/ssl/certs/ca-certificates.crt',
+        ca_path: "/etc/ssl/certs"
+    }}
+  }
+end
