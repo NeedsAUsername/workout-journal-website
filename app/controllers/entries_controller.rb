dc268977410637d429ceeb295d59299bd349df6f
NeedsAUsername <kevinchi118@gmail.com>
2018-09-19 22:26:17 -0400

    entry form creates an entry and entry index page shows entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,22 @@
 class EntriesController < ApplicationController
 
   def create
-    redirect_to root_path
+    @entry = current_user.journal.entries.build(entry_params)
+    if @entry.save
+      redirect_to journal_entries_path
+    else
+      flash[:message] = 'error: entry could not save'
+      redirect_to journals_path
+    end
+  end
+
+  def index
+    @journal = current_user.journal
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:date, :comments)
   end
 end
