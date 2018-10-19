1cd49f7305c25a8684392c6c7aa662bf7983ee45
NeedsAUsername <kevinchi118@gmail.com>
2018-10-19 17:11:13 -0400

    changes button color

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 2018_10_18_230003) do
     t.integer "sets"
     t.integer "reps"
     t.boolean "standard"
-    t.integer "entry_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
