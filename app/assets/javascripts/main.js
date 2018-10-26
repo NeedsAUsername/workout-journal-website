713561bee8101d64e3ce55886e0fb43417c9ce6f
NeedsAUsername <kevinchi118@gmail.com>
2018-10-25 23:10:13 -0400

    creates media query and JS for side nav toggle display

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,6 +2,16 @@
 function hideElement(element) {
   element.style.display = 'none';
 }
+function toggleElement(element, style) {
+  const mq = window.matchMedia( "(max-width: 1200px)" );
+  if (mq.matches) {
+    if (element.style.display === style) {
+      element.style.display = 'none';
+    } else {
+      element.style.display = style;
+    }
+  }
+}
 
 function hideClass(className) {
   let classes = document.querySelectorAll(className);
